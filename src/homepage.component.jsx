698943939e66992ce124635b668b926409369ef7
Jose Alvarez <alvarez_3993@hotmail.com>
2022-03-09 15:48:35 -0600

    feat(Items): Added node-sass and create items

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './homepage.styles.scss'
 
 export default function HomePage(){
     const items = [
@@ -11,17 +12,17 @@ export default function HomePage(){
 
     return (
         <div className="homepage">
-            <div className="directory-menu">
-                <div className="menu-item">
-                {
-                    items.map(({title}) => (
+            <div className="directory-menu">   
+            {
+                items.map(({title}) => (
+                    <div className="menu-item">
                         <div className="content">
                             <h1 className="title">{title}</h1>
                             <span className="subtitle">Shop Now</span>
                         </div>
-                    ))
-                }
-                </div>
+                    </div>
+                ))
+            }
             </div>
         </div>
     );
