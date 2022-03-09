9206876e5ca083f3399a42527e8734a702f31d80
Jose Alvarez <alvarez_3993@hotmail.com>
2022-03-09 17:54:02 -0600

    feat(style): finish styling menu component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import './menu-item.styles.scss'
 
-export default function MenuItem({title}){
+export default function MenuItem({ title, imageUrl, size }){
     return (
-        <div className="menu-item">
+        <div
+            className={classNames('menu-item', { [size]: !!size })}
+        >
+            <div 
+                className="background-image"
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}
+            />
             <div className="content">
                 <h1 className="title">{title}</h1>
                 <span className="subtitle">Shop Now</span>
